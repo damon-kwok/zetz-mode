e9cd1f8257ff66a60f4a77d20f6b6211d234558e
damon-kwok <damon-kwok@outlook.com>
2020-08-03 21:47:44 +0800

    clean

--- zetz-mode.el
+++ zetz-mode.el
@@ -33,28 +33,6 @@
 ;;
 ;;   (require 'zetz-mode)
 ;;
-;;; License:
-;;
-;; Permission is hereby granted, free of charge, to any person
-;; obtaining a copy of this software and associated documentation
-;; files (the "Software"), to deal in the Software without
-;; restriction, including without limitation the rights to use, copy,
-;; modify, merge, publish, distribute, sublicense, and/or sell copies
-;; of the Software, and to permit persons to whom the Software is
-;; furnished to do so, subject to the following conditions:
-;;
-;; The above copyright notice and this permission notice shall be
-;; included in all copies or substantial portions of the Software.
-;;
-;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-;; EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-;; MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-;; NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-;; BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-;; ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-;; CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-;; SOFTWARE.
-
 ;;; Code:
 
 (require 'cl-lib)
@@ -179,6 +157,9 @@
      ;; preprocessor
      (,zetz-preprocessor-keywords-regexp . font-lock-preprocessor-face)
 
+     ;; keywords
+     (,zetz-keywords-regexp . font-lock-keyword-face) ;;font-lock-keyword-face
+
      ;; delimiter: modifier
      ("\\(->\\|=>\\|\\.>\\|:>\\|::\\||\\)" 1 'font-lock-keyword-face)
 
@@ -229,9 +210,6 @@
 
      ;; tuple references
      ("[.]$?[ \t]?\\($?_[1-9]$?[0-9]?*\\)" 1 'font-lock-variable-name-face)
-
-     ;; keywords
-     (,zetz-keywords-regexp . font-lock-keyword-face) ;;font-lock-keyword-face
 
      ;; character literals
      ("\\('[\\].'\\)" 1 'font-lock-constant-face)
